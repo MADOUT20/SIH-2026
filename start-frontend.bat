ac94e373044f68ae92be97e86d28d68cdfb0cb66
mudraa-tayade <yoyoinkybunnypogo@gmail.com>
2026-09-02 00:11:41 +0530

    Delete start-frontend.bat

--- start-frontend.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0frontend" 2>nul || cd /d "%~dp0SIH-2026-DATAXAI\frontend"
-echo ===================================================
-echo Starting NetGuard Frontend on http://localhost:3000
-echo ===================================================
-npm run dev
-pause
